d04c22509a8e3d368c2ba49786ae1ff7f39f7868
runfourestrun <90913666+runfourestrun@users.noreply.github.com>
2022-01-12 20:25:38 -0700

    Kind of works. There is a serious issue with the transactions though. I have to run the script twice for some reason..

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import spacy
 from neo4j import GraphDatabase
 import os
+from spacy.cli.download import download
+
 
 
 
@@ -31,6 +33,45 @@ def batch_parameters(parameters:list,batch_size:int):
     return chunks
 
 
+def ship_batch(chunk):
+    batch = {}
+    batch['batch'] = chunk
+    return batch
+
+
+
+def get_unique_nouns_and_verbs(record):
+    for properties in record:
+        text = properties.get('text')
+        id = properties.get('id')
+    nouns = []
+    verbs = []
+    #download(model='en_core_web_sm')
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(text)
+    for token in doc:
+        if token.pos_ == 'NOUN':
+            nouns.append(token.text)
+        if token.pos_ == 'VERB':
+            verbs.append(token.text)
+
+    unique_nouns = {id:set(nouns)}
+    unique_verbs = {id:set(verbs)}
+
+    data = (unique_verbs,unique_nouns)
+    return data
+
+
+def process_words_to_cypher_parameters(word_dict, word_type = None):
+    parameters = []
+    acceptable_word_types = ['noun','verb']
+    if word_type.lower() in acceptable_word_types:
+        for key,value in word_dict.items():
+            for word in value:
+                parameter = {'origin':key,'word':word,'type':word_type}
+                parameters.append(parameter)
+    return parameters
+
 
 
 def create_document_in_neo(tx, batch):
@@ -57,7 +98,22 @@ def read_document_in_neo(tx):
 
 
 
+def write_nouns(tx, batch):
+    tx.run(
+        '''
+        UNWIND $batch as param
+        MERGE (n:Noun {id:param.word})
+        MERGE (d:Document {id:param.origin}) - [:CONTAINS] -> (n)
+        ''',parameters=batch)
 
+def write_verbs(tx, batch):
+    tx.run(
+        '''
+        UNWIND $batch as param
+        MERGE (v:Verb {id:param.word})
+        MERGE (d:Document {id:param.origin}) - [:CONTAINS] -> (v)
+        '''
+    ,parameters=batch)
 
 
 
@@ -72,25 +128,72 @@ if __name__ == '__main__':
     neo4j_uri = 'bolt://localhost:7687'
     neo4j_database = 'NLP'
     neo4j_driver = GraphDatabase.driver(neo4j_uri, auth=('neo4j','Reddit123!'))
-    nlp = spacy.load('en_core_web_md')
+
+
+
+    document_parameters =  get_document(input_directory)
+    document_batches = batch_parameters(document_parameters,500)
+
+    final_document_batches = list(map(ship_batch,document_batches))
 
 
 
 
 
 
-    parameters =  get_document(input_directory)
-    batches = batch_parameters(parameters,500)
 
 
     with neo4j_driver.session() as session:
-        for _batch in batches:
-            batch = {}
-            batch['batch'] = _batch
-
+        records = session.read_transaction(read_document_in_neo)
+        for batch in final_document_batches:
             session.write_transaction(create_document_in_neo,batch)
 
-        texts = session.read_transaction(read_document_in_neo)
+
+
+
+
+
+    for record in records:
+        unique_verbs,unique_nouns = get_unique_nouns_and_verbs(record)
+        verb_parameters = process_words_to_cypher_parameters(unique_verbs,word_type='verb')
+        noun_parameters = process_words_to_cypher_parameters(unique_nouns,word_type='noun')
+
+        noun_batches = batch_parameters(noun_parameters,500)
+        final_noun_batches = list(map(ship_batch,noun_batches))
+
+
+
+        verb_batches = batch_parameters(verb_parameters,500)
+        final_verb_batches = list(map(ship_batch,verb_batches))
+
+
+
+
+        with neo4j_driver.session() as session:
+            for batch in final_noun_batches:
+                session.write_transaction(write_nouns,batch)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
